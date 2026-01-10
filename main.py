f03d8d6f67fa71261393b70b451eafe2d98005e7
Excies11 <144028435+Excies11@users.noreply.github.com>
2026-01-10 15:49:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import asyncio
 import discord
 from discord.ext import commands
 
-# ================= INTENTS =================
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
+# ================= BASE INTENTS =================
+base_intents = discord.Intents.default()
+base_intents.members = True
+base_intents.message_content = True
 
 # ================= LOG BOT =================
 log_bot = commands.Bot(
     command_prefix="!",
-    intents=intents,
+    intents=base_intents,
     help_command=None
 )
 
@@ -22,39 +22,14 @@ async def on_ready():
 async def start_log_bot():
     await log_bot.load_extension("cogs.logs")
     await log_bot.start(os.getenv("LOG_TOKEN"))
-print("selam")
+
+
 # ================= MOD BOT =================
 mod_bot = commands.Bot(
     command_prefix="!",
-    intents=intents,
+    intents=base_intents,
     help_command=None
 )
-# ================= MUSIC BOT =================
-intents = discord.Intents.default()
-intents.message_content = True
-intents.voice_states = True
-
-music_bot = commands.Bot(
-    command_prefix="!",
-    intents=intents,
-    help_command=None
-)
-
-@music_bot.event
-async def on_ready():
-    print(f"🎵 MUSIC BOT AKTİF: {music_bot.user}")
-
-async def main():
-    await music_bot.load_extension("cogs.music")
-    await music_bot.start(os.getenv("MUSIC_TOKEN"))
-
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
-
-
-
-
 
 @mod_bot.event
 async def on_ready():
@@ -64,10 +39,11 @@ async def start_mod_bot():
     await mod_bot.load_extension("cogs.mod")
     await mod_bot.start(os.getenv("MOD_TOKEN"))
 
+
 # ================= MLOG BOT =================
 mlog_bot = commands.Bot(
     command_prefix="?",
-    intents=intents,
+    intents=base_intents,
     help_command=None
 )
 
@@ -78,6 +54,26 @@ async def on_ready():
 async def start_mlog_bot():
     await mlog_bot.load_extension("cogs.mlog")
     await mlog_bot.start(os.getenv("MLOG_TOKEN"))
+
+
+# ================= MUSIC BOT =================
+music_intents = discord.Intents.default()
+music_intents.message_content = True
+music_intents.voice_states = True
+
+music_bot = commands.Bot(
+    command_prefix="!",
+    intents=music_intents,
+    help_command=None
+)
+
+@music_bot.event
+async def on_ready():
+    print(f"🎵 MUSIC BOT AKTİF: {music_bot.user}")
+
+async def start_music_bot():
+    await music_bot.load_extension("cogs.music")
+    await music_bot.start(os.getenv("MUSIC_TOKEN"))
 
 
 # ================= MAIN =================
