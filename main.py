d51d8282f1fc49f554e0ce120f850d57f2211609
Excies11 <144028435+Excies11@users.noreply.github.com>
2026-01-13 17:09:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,25 @@ mlog_bot = commands.Bot(command_prefix="?", intents=intents)
 music_bot = commands.Bot(command_prefix="!", intents=intents)
 
 # ===== MINECRAFT BOT =====
-minecraft_bot = commands.Bot(command_prefix="!", intents=intents)
+intents = discord.Intents.default()
+intents.message_content = True
 
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents,
+    help_command=None
+)
+
+@bot.event
+async def on_ready():
+    print(f"🟢 BOT AKTİF: {bot.user}")
+
+async def main():
+    await bot.load_extension("cogs.minecraft")
+    await bot.start(os.getenv("MC_TOKEN"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
 async def main():
     await asyncio.gather(
